ead33993b35cc8f66dfcc0571de0297a4ff5b31f
RJ <radoslawjurczak95@gmail.com>
2017-04-06 16:58:57 +0200

    Further mavenization

---
+++ src/main/courses/Course.java
@@ -0,0 +1,15 @@
+package main.courses;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by RJ on 2017-04-06.
+ */
+
+@Entity
+public class Course {
+
+    private String courseName;
+    private List<>
+
+}
