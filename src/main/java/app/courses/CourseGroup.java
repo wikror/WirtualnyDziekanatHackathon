0f5d69053438ba44701986b2198d57853f5e6d67
wikror <wiktor.rorot@gmail.com>
2017-04-06 17:34:56 +0200

    Student initial commit

--- src/main/java/app/courses/CourseGroup.java
+++ src/main/java/app/courses/CourseGroup.java
@@ -1,7 +1,30 @@
 package app.courses;
 
+import app.participants.Student;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by RJ on 2017-04-06.
  */
+
+@Entity
+@Table(name = "GROUP")
 public class CourseGroup {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "STUDENTS")
+    private List<Student> students = new ArrayList<>();
+
+    @Column(name = "GRADES")
+    private Map<Student, Integer> grades = new HashMap<>();
+
 }
